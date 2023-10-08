d00c6e69cda890ae79224e63c7cfee44874e23cc
NoeliaGCalligaro <noeliacalligaro@gmail.com>
2023-10-08 19:58:49 -0300

    Create notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-function Homepage() {
+"use client";
+
+import NoteForm from "@/components/NoteForm";
+import NoteCard from "@/components/NoteCard";
+import { useNotes } from "@/context/NoteContext";
+import { useEffect } from "react";
+
+function HomePage() {
+  const { notes, loadNotes } = useNotes();
+
+  useEffect(() => {
+    loadNotes();
+  }, []);
+
   return (
-    <div>Inicio</div>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <NoteForm />
+
+        {notes.map((note) => (
+          <NoteCard note={note} key={note.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Homepage
+export default HomePage;
